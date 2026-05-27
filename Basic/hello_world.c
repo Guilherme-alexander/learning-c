6efd5a5b9fa079523445d69fd90cea9258339d88
Guilherme Alexander <80430814+Guilherme-alexander@users.noreply.github.com>
2026-05-27 12:42:46 -0300

    Enhance hello_world.c with comments on main function
    Added comments explaining the main function and command line arguments.

--- Basic/hello_world.c
+++ Basic/hello_world.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 // Basic C (Hello, World!)
+// int main(void) = a função recebe zero argumentos (compilador pode aceitar dependendo do modo.)
+// int main() = não especifica quais argumentos recebe (não aceita parâmetros)
+// com argumentos do terminal
+
+
+// programa.exe teste.txt
+
+
+// int main(int argc, char *argv[])
+// argc = 2
+// argv[0] = programa.exe
+// argv[1] = teste.txt
 
 int main(void) {
     printf("Hello, World!\n");
